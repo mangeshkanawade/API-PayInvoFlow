0e5091cf0585a46fd9dc093e96fe02b9a9bf020a
mangeshkanawade <kanawade84@gmail.com>
2025-09-11 01:35:41 +0530

    Template Updated, Code Optimization

---
+++ API/src/helper/pdfGenerator.ts
@@ -0,0 +1,60 @@
+import * as fs from "fs";
+import path from "path";
+
+import chromium from "@sparticuz/chromium";
+import Handlebars from "handlebars";
+import puppeteer from "puppeteer";
+import puppeteerCore, { LaunchOptions } from "puppeteer-core";
+
+export async function pdfGenerator(data: any): Promise<Buffer> {
+  try {
+    // 1. Load HTML template
+    const templatePath = path.join(__dirname, "../views/template.html");
+    const htmlTemplate = await fs.promises.readFile(templatePath, "utf-8");
+
+    // 2. Compile template with Handlebars
+    const template = Handlebars.compile(htmlTemplate);
+    const html = template({
+      ...data,
+    });
+
+    // 3. launch
+    const isProd =
+      process.env.VERCEL_ENV === "production" ||
+      process.env.NODE_ENV === "production";
+    const browser = isProd
+      ? await puppeteerCore.launch({
+          args: [...chromium.args, "--disable-gpu", "--no-sandbox"],
+          defaultViewport: {
+            width: 1280,
+            height: 800,
+            deviceScaleFactor: 1,
+            isMobile: false,
+            hasTouch: false,
+            isLandscape: false,
+          },
+          executablePath: await chromium.executablePath(), // note: function call
+          headless: "shell",
+        } satisfies LaunchOptions)
+      : await puppeteer.launch({ channel: "chrome", headless: true });
+
+    const page = await browser.newPage();
+    // 4. Set HTML content
+    await page.setContent(html, { waitUntil: "load" });
+
+    // 5. Generate PDF buffer
+    const pdfBuffer = Buffer.from(
+      await page.pdf({
+        format: "A4",
+        printBackground: true,
+      })
+    );
+
+    await browser.close();
+
+    return pdfBuffer;
+  } catch (err) {
+    console.error("PDF generation failed:", err);
+    throw new Error("PDF generation failed. " + err);
+  }
+}
--- API/src/services/invoice.service.ts
+++ API/src/services/invoice.service.ts
@@ -1,16 +1,10 @@
 import * as fs from "fs";
 import path from "path";
+import { pdfGenerator } from "../helper/pdfGenerator";
 import { IInvoice, InvoiceModel } from "../models/invoice.model";
 import { InvoiceItemModel } from "../models/invoiceitem.model";
 import { BaseRepository } from "../repositories/base.repository";
 import { BaseService } from "../services/base.service";
-
-import chromium from "@sparticuz/chromium";
-import Handlebars from "handlebars";
-import puppeteer from "puppeteer";
-import puppeteerCore, { LaunchOptions } from "puppeteer-core";
-// import puppeteerCore from "puppeteer-core";
-
 export class InvoiceService extends BaseService<IInvoice> {
   constructor(public repo: BaseRepository<IInvoice>) {
     super(repo);
@@ -24,16 +18,55 @@ export class InvoiceService extends BaseService<IInvoice> {
     return InvoiceModel.findByIdAndUpdate(id, { status }, { new: true });
   }
 
-  buildInvoiceData() {
+  async exportPdf(id: string): Promise<Buffer> {
+    return Buffer.alloc(0);
+  }
+
+  async getByClient(clientId: string) {
+    return InvoiceModel.find({ client: clientId }).populate("client company");
+  }
+
+  async getByCompany(companyId: string) {
+    return InvoiceModel.find({ company: companyId }).populate("client company");
+  }
+
+  // Item Handling (moved to InvoiceItemModel)
+  async addItem(invoiceId: string, item: any) {
+    return InvoiceItemModel.create({ ...item, invoice: invoiceId });
+  }
+
+  async updateItem(invoiceId: string, itemId: string, item: any) {
+    return InvoiceItemModel.findOneAndUpdate(
+      { _id: itemId, invoice: invoiceId },
+      item,
+      { new: true }
+    );
+  }
+
+  async removeItem(invoiceId: string, itemId: string) {
+    return InvoiceItemModel.findOneAndDelete({
+      _id: itemId,
+      invoice: invoiceId,
+    });
+  }
+
+  async exportPdfFile(): Promise<Buffer> {
+    const invoiceData = await this.buildInvoiceData();
+    return await pdfGenerator(invoiceData);
+  }
+
+  async buildInvoiceData() {
+    const logoPath = path.join(__dirname, "../images/logo.png");
+    const logoBuffer = await fs.promises.readFile(logoPath);
+    const logoBase64 = logoBuffer.toString("base64");
+
     return {
       provider: {
         name: "Ansh Enterprises",
         gstin: "27BRFPK4610M1ZN",
         address: "A/p Karegaon MIDC, Tal - Shirur, Dist - Pune 412220",
         state: "Maharashtra",
-        // Use either a base64 logo or leave empty to show the placeholder
-        logo: "", // data URI or file path not used here; placeholder used in template
-        // logoBase64: "" // if using data:image/png;base64,...
+        logo: logoBase64,
       },
       receiver: {
         name: "Hora Art Centre Pvt Ltd.",
@@ -96,114 +129,5 @@ export class InvoiceService extends BaseService<IInvoice> {
           "One Lakh Nineteen Thousand Eight Hundred Forty Four Rupees Only",
       },
     };
-  }
-
-  async exportPdf(id: string): Promise<Buffer> {
-    return Buffer.alloc(0);
-  }
-
-  async getByClient(clientId: string) {
-    return InvoiceModel.find({ client: clientId }).populate("client company");
-  }
-
-  async getByCompany(companyId: string) {
-    return InvoiceModel.find({ company: companyId }).populate("client company");
-  }
-
-  // Item Handling (moved to InvoiceItemModel)
-  async addItem(invoiceId: string, item: any) {
-    return InvoiceItemModel.create({ ...item, invoice: invoiceId });
-  }
-
-  async updateItem(invoiceId: string, itemId: string, item: any) {
-    return InvoiceItemModel.findOneAndUpdate(
-      { _id: itemId, invoice: invoiceId },
-      item,
-      { new: true }
-    );
-  }
-
-  async removeItem(invoiceId: string, itemId: string) {
-    return InvoiceItemModel.findOneAndDelete({
-      _id: itemId,
-      invoice: invoiceId,
-    });
-  }
-
-  async exportPdfFile(): Promise<Buffer> {
-    try {
-      // 1. Load HTML template
-      const templatePath = path.join(__dirname, "../views/template.html");
-      const htmlTemplate = await fs.promises.readFile(templatePath, "utf-8");
-
-      // 2. Compile template with Handlebars
-      const template = Handlebars.compile(htmlTemplate);
-      const val = this.buildInvoiceData();
-      const html = template({
-        ...val,
-      });
-
-      // 3. Launch Puppeteer
-      // const browser = await puppeteer.launch({
-      //   args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      // });
-      // const page = await browser.newPage();
-
-      // Fallback for executablePath
-
-      // const executablePath =
-      //   process.env.NODE_ENV === "production"
-      //     ? (await chromium.executablePath) || "/usr/bin/chromium-browser"
-      //     : puppeteer.executablePath();
-
-      // const browser = await puppeteer.launch({
-      //   args: process.env.NODE_ENV === "production" ? chromium.args : [],
-      //   defaultViewport: chromium.defaultViewport,
-      //   // executablePath,
-      //   executablePath: await chromium.executablePath,
-      //   headless: true,
-      // });
-
-      // const page = await browser.newPage();
-
-      // 3. launch
-      const isProd =
-        process.env.VERCEL_ENV === "production" ||
-        process.env.NODE_ENV === "production";
-      const browser = isProd
-        ? await puppeteerCore.launch({
-            args: [...chromium.args, "--disable-gpu", "--no-sandbox"],
-            defaultViewport: {
-              width: 1280,
-              height: 800,
-              deviceScaleFactor: 1,
-              isMobile: false,
-              hasTouch: false,
-              isLandscape: false,
-            },
-            executablePath: await chromium.executablePath(), // note: function call
-            headless: "shell",
-          } satisfies LaunchOptions)
-        : await puppeteer.launch({ channel: "chrome", headless: true });
-
-      const page = await browser.newPage();
-      // 4. Set HTML content
-      await page.setContent(html, { waitUntil: "load" });
-
-      // 5. Generate PDF buffer
-      const pdfBuffer = Buffer.from(
-        await page.pdf({
-          format: "A4",
-          printBackground: true,
-        })
-      );
-
-      await browser.close();
-
-      return pdfBuffer;
-    } catch (err) {
-      console.error("PDF generation failed:", err);
-      throw new Error("PDF generation failed. " + err);
-    }
   }
 }
